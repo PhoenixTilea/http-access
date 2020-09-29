5f1d64020dd67c19ab7f13c4f70b08c7701000c1
Sabelyn Thorpe[B[D[C[C[D[D[D[D[D[D[D[C[C[C[C[C[C[A[B[B[B[B[B[C[C[D[D[D[D[C[C[C[Sabelyn Thorpe <sabelyn@outlook.com>
2020-09-29 17:23:46 -0400

    fixed another routing issue

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
 	}
 });
 app.use("/api", require("./routes/reqRouter"));
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
 })
 
