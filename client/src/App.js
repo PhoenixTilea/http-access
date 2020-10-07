708da216d4beac5cd0f378e031d1cfe2ad87c197
Sabelyn Thorpe[B[D[C[C[D[D[D[D[D[D[D[C[C[C[C[C[C[A[B[B[B[B[B[C[C[D[D[D[D[C[C[C[Sabelyn Thorpe <sabelyn@outlook.com>
2020-10-07 19:16:03 -0400

    Added footer to link to Github

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import RequestForm from "./components/RequestForm";
 import ResponseDisplay from "./components/ResponseDisplay";
+import Footer from "./components/Footer";
 import { RequestContextProvider } from "./RequestContext";
 import ThemeForm from "./components/ThemeForm";
 import "./themes.css";
@@ -23,6 +24,7 @@ export default function App() {
 				<RequestForm />
 				<ResponseDisplay />
 			</main>
+			<Footer />
 		</div></RequestContextProvider>
 	);
 }
---
+++ client/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Footer() {
+	return (
+		<footer>
+			<p>
+				For more information and instructions on running the app locally (for testing locally hosted servers), check this out on 
+				<a href="https://github.com/phoenixtilea/http-access" target="_new">GitHub</a>.
+			</p>
+		</footer>
+	);
+}
